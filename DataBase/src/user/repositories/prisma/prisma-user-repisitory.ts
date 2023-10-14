6e36493ed0fbe1ffbe3383a7688b98e1d4a18004
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-15 00:20:08 +0100

    Back End - Users Repository, Creation finf by email

--- DataBase/src/user/repositories/prisma/prisma-user-repisitory.ts
+++ DataBase/src/user/repositories/prisma/prisma-user-repisitory.ts
@@ -12,4 +12,14 @@ export class PrismaUsersRepository implements UsersRepository{
     return user
   }
 
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        },
+    });
+
+    return user;
+  }
+
 }
