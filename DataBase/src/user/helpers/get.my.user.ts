9b3eb271624ff8614ef2e6d40d14a824e2228bf1
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-17 14:15:34 +0100

    Back End - getMyuser function

---
+++ DataBase/src/user/helpers/get.my.user.ts
@@ -0,0 +1,22 @@
+import { ForbiddenException } from '@nestjs/common';
+import { Request } from 'express';
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-user-repisitory';
+
+export async function getMyUser(id: string, req: Request) {
+  
+  const usersRepository = new PrismaUsersRepository();
+
+  const user = await usersRepository.findUserById(id);
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const foundUser = req.user as {id: string};
+
+  if (user.id !== foundUser.id) {
+    throw new ForbiddenException('You do not have permission to access this user.');
+  }
+
+  return { user };
+}
--- DataBase/src/user/user.controller.ts
+++ DataBase/src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  getMyUser(@Param() params: { id: string }, @Req() req: Request) {
+    return this.userService.getMyUser(params.id, req);
+  }
 }
