c1677681de84ad7386996243d3c658d32846992a
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-17 13:29:12 +0100

    Control Everyting Admin - Creation of index page

--- Control_Everything_Admin/src/App.jsx
+++ Control_Everything_Admin/src/App.jsx
@@ -1,12 +1,19 @@
 // React and Scss
   import React from 'react';
+  import { Route, Routes, useLocation } from 'react-router-dom';
 // React and Scss
+
+// Internal Components
+  import * as Public_Page from './Imports/public.pages';
+// Internal Components
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route index element={ <Public_Page.Index /> } />
+      </Routes>
     </>
   )
 }
---
+++ Control_Everything_Admin/src/Pages/Public/Index/Index.jsx
@@ -0,0 +1,13 @@
+// React and scss
+  import React from 'react';
+// React and scss
+
+const Index = () => {
+  return (
+    <>
+      Teste
+    </>
+  )
+};
+
+export default Index;
