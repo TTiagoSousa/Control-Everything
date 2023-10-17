1cdd1046697916bc1d250de00c19d5f6090a9427
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-17 07:01:32 +0100

    Back End - GetCoutries Function Base

--- DataBase/src/coutries/coutries.controller.ts
+++ DataBase/src/coutries/coutries.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { CoutriesService } from './coutries.service';
 
 @Controller('coutries')
 export class CoutriesController {
   constructor(private readonly coutriesService: CoutriesService) {}
+
+  @Post('fetch-data')
+  async GetCoutries() {
+    await this.coutriesService.GetCoutries();
+    return { message: 'Moeda inserida com sucesso' };
+  }
+
 }
--- DataBase/src/coutries/coutries.service.ts
+++ DataBase/src/coutries/coutries.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { GetCoutries } from './helpers/get.countries';
 
 @Injectable()
-export class CoutriesService {}
+export class CoutriesService {
+
+  async GetCoutries() {
+    const result = await GetCoutries();
+    return result;
+  }
+
+}
