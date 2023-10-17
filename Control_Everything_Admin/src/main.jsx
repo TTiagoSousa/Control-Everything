89e7f6192bef2aed86a07b94b391804e069129de
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-17 10:13:28 +0100

    Control Everyting Admin - React Router Dom Config

--- Control_Everything_Admin/src/main.jsx
+++ Control_Everything_Admin/src/main.jsx
@@ -2,10 +2,11 @@
   import React from 'react';
   import ReactDOM from 'react-dom/client';
   import App from './App.jsx';
+  import { BrowserRouter } from 'react-router-dom';
 // React and Scss
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>,
+  <BrowserRouter>  
+    <App />
+  </BrowserRouter>,
 )
