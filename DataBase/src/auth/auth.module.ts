00618fbdc0f08f1739c8d4e1c74d841caed2b4b4
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-18 03:40:28 +0100

    Back End - auth, jwt config

--- DataBase/src/auth/auth.module.ts
+++ DataBase/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CoutriesModule } from 'src/coutries/coutries.module';
 import { CoutriesService } from 'src/coutries/coutries.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtSecret } from 'src/utils/constants';
 
 @Module({
-  imports: [CoutriesModule],
+  imports: [
+    CoutriesModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret, // Replace with your actual secret key
+      signOptions: { expiresIn: '1d' }, // Token expiration
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, CoutriesService],
 })
