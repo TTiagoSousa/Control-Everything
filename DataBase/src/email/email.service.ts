436e6550b8db48a7235cf0e0d3ec5f9588a818b4
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-18 03:46:14 +0100

    Back End - Email, transporter config

--- DataBase/src/email/email.service.ts
+++ DataBase/src/email/email.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter: { sendMail: (arg0: { from: string; to: string; subject: string; html: string; }) => any; };
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      // Configuração do serviço de e-mail aqui (por exemplo, Gmail)
+      service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+}
