d728f2429c402c4d85ab2f331345e5fc035dce85
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-21 01:00:48 +0100

    Control Everything - Creation of index page and global styles.scss

--- Control_Everything/src/App.jsx
+++ Control_Everything/src/App.jsx
@@ -1,14 +1,23 @@
 // React and Scss
   import React from 'react';
+  import './Styles/Global.scss'
+  import { Route, Routes, useLocation } from 'react-router-dom';
 // React and Scss
+
+// Internal Components
+  import * as Public_Page from './Imports/public.pages';
+// Internal Components
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route index element={ <Public_Page.Index /> } />
+        <Route path='Login' element={ <Public_Page.Index /> } />
+      </Routes>
     </>
   )
-};
+}
 
 export default App;
---
+++ Control_Everything/src/Pages/Public/Index/Index.jsx
@@ -0,0 +1,12 @@
+// React and Scss
+  import React from 'react';
+  import './Index.scss';
+// React and Scss
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index;
