4a139dae6af4a8814e709adbf3e0bfa967e901ff
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-23 08:21:04 +0100

    Control Everyting - Thoggle theme on custumize sidebar and mobile version

--- Control_Everything/src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
+++ Control_Everything/src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
@@ -5,14 +5,18 @@
 
 // Internal Imports
   import * as Component from '../../../Imports/components';
+  import * as Icon from '../../../Imports/icons';
+  import * as Color from '../../../Styles/Colors';
 // Internal Imports
 
 // Contexts
   import { NavsState } from '../../../Contexts/Navs_Context';
+  import { ThemeState } from '../../../Contexts/Theme_Context';
 // Contexts
 
 const Custumize_Sidebar = () => {
 
+  const { handleDarkMode, handleLightMode, handleAutoMode, mode } = ThemeState();
   const { customize_Sidebar, setCustomize_Sidebar, showCustomize_Sidebar } = NavsState();
 
   return (
@@ -23,7 +27,66 @@ const Custumize_Sidebar = () => {
       />
 
       <nav className={`Custumize_Sidebar ${customize_Sidebar ? 'active' : ''}`}>
-        <span>teste</span>
+        <header>
+          <div className='Circle_N1'></div>
+          <div className='Circle_N2'></div>  
+          <div className="Container">
+            <div className="Top">
+              <div className="Left">
+                <div className='Icon' >
+                  <Icon.Painting 
+                    GlobalColor={Color.whitte}
+                  />
+                </div>
+                <h1>Settings</h1>
+                <button>
+                  <div>
+                    <Icon.Back_Square 
+                      GlobalColor={Color.whitte}
+                    />
+                  </div>
+                  <span>Reset</span>
+                </button>
+              </div>
+              <div className="Right">
+                <div 
+                  className='Close_Icon'
+                  onClick={showCustomize_Sidebar}
+                >
+                  <div className='Line_N1'></div>
+                  <div className='Line_N2'></div>
+                </div>
+              </div>
+            </div>
+            <div className="Bottom">
+              <span>Set your own customized style</span>
+            </div>
+          </div>
+        </header>
+
+        <div className='Settings_Container'>
+
+          {/* Chosse the theme */}
+            <div className='Toggle_Theme'>
+              <div className='Text'>
+                <h1>Color Scheme</h1>
+                <span>Choose the perfect color mode for your app.</span>
+              </div> 
+              <div className='Type_Of_Modes'>
+                <div className={`Dark ${mode === 'dark' ? 'GreenBoxShadow' : ''}`} onClick={handleDarkMode}> 
+                  <span>Dark Mode</span>
+                </div>
+                <div className={`Ligth ${mode === 'light' ? 'GreenBoxShadow' : ''}`} onClick={handleLightMode}>
+                  <span>Light Mode</span>
+                </div>
+                <div className={`Auto ${mode === 'auto' ? 'GreenBoxShadow' : ''}`} onClick={handleAutoMode}>
+                  <span>Auto Mode</span>
+                </div>
+              </div>
+            </div>
+          {/* Chosse the theme */}
+
+        </div>
       </nav>
     </>
   )
--- Control_Everything/src/Imports/icons.js
+++ Control_Everything/src/Imports/icons.js
@@ -1,3 +1,7 @@
+// Arrow
+  export { default as Back_Square } from '../Assets/Icons/Arrow/Back_Square';
+// Arrow
+
 // Circle
   export { default as Circle_Half } from '../Assets/Icons/Circle/Circle_Half';
 // Circle
@@ -9,6 +13,10 @@
 // Moon
   export { default as Moon_N1 } from '../Assets/Icons/Moon/Moon_N1';
 // Moon
+
+// Painting
+  export { default as Painting } from '../Assets/Icons/Painting/Painting';
+// Painting
 
 // Settings
   export { default as Settings_With_Two_Arrows } from '../Assets/Icons/Settings/Settings_With_Two_Arrows';
