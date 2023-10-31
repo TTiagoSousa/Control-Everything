5bc07c9a8de95c3d3c0b9412e3a73209222874ef
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-31 06:28:30 +0000

    Admin - DataBase Context

---
+++ Control_Everything_Admin/src/Contexts/DataBase_Context.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const DataBase = createContext({});
+
+const DataBaseContext = ({ children }) => {
+
+  const navigate = useNavigate();
+
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [gender, setGender] = useState('');
+  const [password, setPassword] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState ('');
+  const [country, setCountry] = useState ('');
+  const [confirmPassword, setConfirmPassword ] = useState('');
+
+  return (
+    <DataBase.Provider 
+      value={{ 
+        fullName, setFullName,
+        email, setEmail,
+        password, setPassword,
+        dateOfBirth, setDateOfBirth,
+        country, setCountry,
+        confirmPassword, setConfirmPassword,
+        gender, setGender,
+      }}
+    >
+      {children}
+    </DataBase.Provider>
+  );
+};
+
+export default DataBaseContext;
+
+export const DataBaseState = () => { 
+  return useContext(DataBase);
+};
+
+
+
+
+
+
--- Control_Everything_Admin/src/main.jsx
+++ Control_Everything_Admin/src/main.jsx
@@ -3,10 +3,13 @@
   import ReactDOM from 'react-dom/client';
   import App from './App.jsx';
   import { BrowserRouter } from 'react-router-dom';
+import DataBaseContext from './Contexts/DataBase_Context.jsx';
 // React and Scss
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>  
-    <App />
+    <DataBaseContext>
+      <App />
+    </DataBaseContext>
   </BrowserRouter>,
 )
