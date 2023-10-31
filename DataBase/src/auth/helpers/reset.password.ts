010419de2a4b0c61506a14df1b38f626df75e0ff
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-10-31 01:16:11 +0000

    Back End - Reset Password function

---
+++ DataBase/src/auth/helpers/reset.password.ts
@@ -0,0 +1,33 @@
+import { PrismaUsersRepository } from "src/user/repositories/prisma/prisma-user-repisitory";
+import { NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { hashPassword, isStrongPassword } from "src/utils/all.utilis";
+import * as Reset_Password_Error from '../../user/errors/recover.passwords.erros';
+
+export async function activateAccount(
+  jwt: JwtService,
+  token: string,
+  newPassword: string
+) {
+
+  const payload = jwt.verify(token);
+  const userEmail = payload.email;
+
+  const usersRepository = new PrismaUsersRepository();
+  const user = await usersRepository.findByEmail(userEmail);
+
+  if (!user) {
+    throw new NotFoundException('User not found.');
+  }
+
+  if (isStrongPassword(newPassword)) {
+    throw new Reset_Password_Error.weak_passowrd
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  await usersRepository.save({
+    ...user,
+    hashedPassword
+  });
+}
