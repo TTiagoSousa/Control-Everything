e021cb76b9055100a9c71922aadc86e45d5a1f38
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-11-01 03:08:12 +0000

    Control Everything - Loading Page

---
+++ Control_Everything/src/Pages/Intermediate_Pages/Loading_Page/Loading_Page.jsx
@@ -0,0 +1,22 @@
+// React More CSS
+  import React from 'react';
+  import './Loading_Page.scss';
+// React More CSS
+
+function Loading_Page() {
+
+  return (
+    <>
+      <main className='Loading_Page'>
+        <div className='Container'>
+          <div className='Title'>
+            <h1>Control Everthing</h1>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+
+};
+
+export default Loading_Page;
