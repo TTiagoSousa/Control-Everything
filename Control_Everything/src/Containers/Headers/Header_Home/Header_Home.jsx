89734ad82ec910ee225095d1de4d4e968aec9591
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-11-01 06:18:48 +0000

    Control Everything - Header home, add hamburger button and theme button

--- Control_Everything/src/Containers/Headers/Header_Home/Header_Home.jsx
+++ Control_Everything/src/Containers/Headers/Header_Home/Header_Home.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import './Header_Home.scss';
 import * as Component from '../../../Imports/components';
 import * as Container from '../../../Imports/containers';
+import * as Color from '../../../Styles/Colors';
+import { Sling as Hamburger } from 'hamburger-react';
 
 const Header_Home = () => {
 
@@ -11,10 +13,17 @@ const Header_Home = () => {
     <>
       <header className='Header_Home'>
         <div className="Left_Side">
-        
+          <div className="Menu">
+            <Hamburger  
+              size={20}
+              color={Color.blue}
+            />
+          </div>
         </div>
         <div className="Right_Side">
-          
+          <div className='Theme_Button'>
+            <Component.Change_Theme />
+          </div>
         </div>
       </header>
     </>
