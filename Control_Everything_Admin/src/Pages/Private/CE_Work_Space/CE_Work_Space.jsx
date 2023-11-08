7001c7e89a7853ea4737e2d577d183aa5a67b754
TTiagoSousa <ttiagopintosousa97@gmail.com>
2023-11-08 11:43:16 +0000

    Admin - Link to exel functions

--- Control_Everything_Admin/src/Pages/Private/CE_Work_Space/CE_Work_Space.jsx
+++ Control_Everything_Admin/src/Pages/Private/CE_Work_Space/CE_Work_Space.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './CE_Work_Space.scss';
+import { Link } from 'react-router-dom';
 
 const CE_Work_Space = () => {
   return (
-    <div>CE_Work_Space</div>
+    <div className='CE_Work_Space'>
+      <div>
+        <span>Function Validation</span>
+        <a 
+          href="https://docs.google.com/spreadsheets/d/16CZDwGP0fykb0kPEoaLqP62Hj3Zn7XxtU6MjgXuhRuQ/edit?usp=drive_link" 
+          target="_blank" 
+          rel="noopener noreferrer"
+       >
+        <button>Link</button>
+      </a>
+      </div>
+    </div>
   )
 };
 
