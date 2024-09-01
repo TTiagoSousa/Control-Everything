6e5145fb98146d233f9060b63bcd2b51d3cf818b
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-01 02:21:44 +0100

    Header Index

---
+++ src/Containers/Headers/Header_Index/Header_Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header_Index.scss';
+
+const Header_Index = () => {
+  return (
+    <div className='Header_Index'>
+      <div className="Left_Side">
+        <span>Control Everytinhg</span>
+      </div>
+      <div className="Right_Side"></div>
+    </div>
+  )
+}
+
+export default Header_Index;
--- src/Pages/Public/Index/Index.jsx
+++ src/Pages/Public/Index/Index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Index.scss';
+import Header_Index from '../../../Containers/Headers/Header_Index/Header_Index';
 
 const Index = () => {
   return (
-    <div className='Index'>Index</div>
+    <div className='Index'>
+
+      <Header_Index />
+
+    </div>
   )
 };
 
