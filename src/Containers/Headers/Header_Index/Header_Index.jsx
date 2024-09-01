c49931e9d847e72333e81f476faf79154483f241
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-01 19:02:35 +0100

    Header index, add auth buttons

--- src/Containers/Headers/Header_Index/Header_Index.jsx
+++ src/Containers/Headers/Header_Index/Header_Index.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import './Header_Index.scss';
 import Change_Theme from '../../Selectors/Change_Theme/Change_Theme';
 import Change_Language_And_Currency from '../../Selectors/Change_Language_And_Currency/Change_Language_And_Currency';
+import { useTranslation } from 'react-i18next';
+import { DataBaseState } from '../../../Contexts/DataBase_Context';
+import Simple_Button from '../../../Components/Buttons/Simple_Button/Simple_Button';
 
 const Header_Index = () => {
+
+  const { t } = useTranslation();
+
+  const { authenticated } = DataBaseState();
+
   return (
     <div className='Header_Index'>
       <div className="Left_Side">
         <span>Control Everytinhg</span>
       </div>
       <div className="Right_Side">
-        <div className="Button_Field">
+        {authenticated ?
+          <></>
+          :
+          <>
+            <div className="Button_Field">
+              <Simple_Button 
+                text={t('Sign in')}
+                buttonStyle='type1'
+              />
+            </div>
+            <div className="Button_Field">
+              <Simple_Button 
+                text={t('Sign up')}
+                buttonStyle='type2'
+              />
+            </div>
+          </>
+        }
+        <div className="Selector_Field">
           <Change_Language_And_Currency />
         </div>
-        <div className='Button_Field'>
+        <div className='Selector_Field'>
           <Change_Theme />
         </div>
       </div>
