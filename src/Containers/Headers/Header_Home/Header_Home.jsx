c97496ea57d1f0f442bd8b8f1c3f1eaaed570255
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-03 10:43:43 +0100

    Header home

---
+++ src/Containers/Headers/Header_Home/Header_Home.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Header_Home.scss';
+import Change_Theme from '../../../Containers/Selectors/Change_Theme/Change_Theme';
+import { ThemeState } from '../../../Contexts/Theme_Context';
+import Change_Language_And_Currency from '../../Selectors/Change_Language_And_Currency/Change_Language_And_Currency';
+
+const Header_Home = () => {
+
+  const { mode } = ThemeState();
+
+  const [hovered, setHovered] = useState(null);
+
+  return (
+    <header className='Header_Home'>
+      <div className="Left_Side">
+      </div>
+      <div className="Right_Side">
+        <div className='Button_Field'>
+          <Change_Theme />
+        </div>
+        <div className='Button_Field'>
+          <Change_Language_And_Currency />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header_Home
--- src/Pages/Private/CE/CE.jsx
+++ src/Pages/Private/CE/CE.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './CE.scss';
 import Private_Routes from '../../../Routes/Private_Routes';
+import Header_Home from '../../../Containers/Headers/Header_Home/Header_Home';
 
 const CE = () => {
 
   return (
     <>
+      <Header_Home />
+
       <div className='CE'>
         <div>
           <Private_Routes />  
