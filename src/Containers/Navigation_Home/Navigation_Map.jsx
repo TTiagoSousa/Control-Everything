e53899933afef27b1cf105c54b9901af20625190
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-04 23:10:24 +0100

    Savings page

--- src/Containers/Navigation_Home/Navigation_Map.jsx
+++ src/Containers/Navigation_Home/Navigation_Map.jsx
@@ -3,16 +3,13 @@ export const navItems = [
 
   { to: '/CE/Dashboard', text: 'Home', icon: <Icon.Dashboard_Cubes /> },
 
-  // {
-  //   text: 'Assets',
-  //   icon: <Icon.Dashboard_Cubes />,
-  //   dropdownItems: [
-  //     { to: '/CE/Crypto_Dashboard', text: 'Crypto' },
-  //     { to: '/CE/Savings_Dashboard', text: 'Savings' },
-  //   ],
-  // },
-  
-  // { to: '/CE/Academy', text: 'Academy', icon: <Icon.Dashboard_Cubes /> },
+  {
+    text: 'Assets',
+    icon: <Icon.Dashboard_Cubes />,
+    dropdownItems: [
+      { to: '/CE/Savings_Dashboard', text: 'Savings' },
+    ],
+  },
 
   {
     text: 'Settings',
---
+++ src/Pages/Private/Assets_Dashboards/Savings/Savings_Dashboard/Savings_Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Savings_Dashboard.scss';
+import { useTranslation } from 'react-i18next';
+
+const Savings_Dashboard = () => {
+
+  const { t } = useTranslation();
+
+  return (
+    <div className='Savings_Dashboard'>
+
+    
+    </div>
+  )
+};
+
+export default Savings_Dashboard;
