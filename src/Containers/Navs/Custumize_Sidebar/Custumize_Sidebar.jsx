bb9cffe64ec8f3750c0be5ef2e0f1c8717d0f650
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-06 14:46:10 +0100

    Custumize sidebar, choose type of navigation

--- src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
+++ src/Containers/Navs/Custumize_Sidebar/Custumize_Sidebar.jsx
@@ -3,10 +3,13 @@ import './Custumize_Sidebar.scss';
 import Dark_Div from '../../../Components/Dark_Div/Dark_Div';
 import { NavsState } from '../../../Contexts/Navs_Context';
 import Header from './Components/Header';
+import Type_Of_Navigations from './Components/Type_Of_Navigations';
 
 const Custumize_Sidebar = () => {
 
   const { customize_Sidebar, setCustomize_Sidebar, typeOfNavifation } = NavsState();
+
+  const Menu_Options_Show = typeOfNavifation === "Mobile_Menu" 
 
   return (
     <>
@@ -19,7 +22,7 @@ const Custumize_Sidebar = () => {
       <nav className={`Custumize_Sidebar ${customize_Sidebar ? 'active' : ''}`}>
         <Header />
         <div className='Settings_Container'>
-          
+          <Type_Of_Navigations />
         </div>
       </nav>
     </>
