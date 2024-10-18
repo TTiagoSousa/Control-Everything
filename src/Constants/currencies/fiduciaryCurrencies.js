44ad8d4c4336eca64190fcbf5737ecd232a6f39a
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-10-18 17:17:26 +0100

    Update currencies id

--- src/Constants/currencies/fiduciaryCurrencies.js
+++ src/Constants/currencies/fiduciaryCurrencies.js
@@ -1,5 +1,5 @@
 export const fiduciaryCurrencies = [
-  { name: 'USD', value: '52f30dcd-2c01-4c99-b982-02b313dfb6e5' },
-  { name: 'EUR', value: '8ed53eb6-b6aa-4bdd-8b29-b6139f511d3b' },
-  { name: 'BRL', value: '2f85c386-ccf5-4cbf-bfb3-26929f57ccd7' }
+  { name: 'USD', value: 'e8993f89-8d00-4bf7-bf8c-8968d394c2ad' },
+  { name: 'EUR', value: '5046d706-240a-4681-ad95-f2a7aae1ff9d' },
+  { name: 'BRL', value: '75a4a606-7316-4bc2-8107-5ad4dd94623a' }
 ];
