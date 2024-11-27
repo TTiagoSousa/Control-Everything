3bb476276ce45de763a0795aeb5ae12e01eb01fb
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-11-27 19:40:28 +0000

    Index Page

---
+++ src/Pages/Public/Index/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Index.scss';
+import { Link } from 'react-router-dom';
+
+const Index = () => {
+
+  return (
+    <div className='Index'>
+      <Link to='Sign_In'>Sign_In</Link>
+    </div>
+  )
+};
+
+export default Index;
